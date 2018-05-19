82c20c36d35f704000d5da09817d72d4046dba0d
Oliver Borchert <oliver.borchert@in.tum.de>
2018-05-19 16:48:28 +0200

    Initial Commit

---
+++ BxSockets/BxSockets.h
@@ -0,0 +1,19 @@
+//
+//  BxSockets.h
+//  BxSockets
+//
+//  Created by Oliver Borchert on 5/19/18.
+//  Copyright © 2018 Oliver Borchert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BxSockets.
+FOUNDATION_EXPORT double BxSocketsVersionNumber;
+
+//! Project version string for BxSockets.
+FOUNDATION_EXPORT const unsigned char BxSocketsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BxSockets/PublicHeader.h>
+
+
